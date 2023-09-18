428814f1ba755160cec77cb0b719fe58a19622fb
ZYPZ <72951371+ZYPZ33@users.noreply.github.com>
2023-09-18 17:54:21 +0100

    Added more code

--- flags.py
+++ flags.py
@@ -4,21 +4,33 @@ from random import choice
 from os import listdir
 
 
-def guess():
-    return True
+def setImageName(directory):
+    imageName = choice(listdir(directory))
+    image = directory + imageName
+    return [imageName, image]
 
 
-directory = "flags/"
-imageName = choice(listdir(directory))
-image = directory + imageName
+def makeWidget(imageName, image):
+    app = App(title="Guess the Flag", width=300, height=400)
+    Text(app, imageName.strip(".jpg"))  # cheat
+    flag = Picture(app, image, width=150, height=100)
+    Text(app, "What country does this flag represent?")
+    inputData = TextBox(app)
+    PushButton(app, text="Submit", command=guess)
+    app.display()
 
-app = App(title="Guess the Flag", width=300, height=400)
-Text(app, imageName.strip(".jpg"))
 
-flag = Picture(app, image, width=150, height=100)
-Text(app, "What country is this flag?")
+def guess(inputData, imageName):
+    if inputData.value.lower() == imageName.strip(".jpg").lower():
+        print("correct!")
+    else:
+        print("Try again!")
 
-inputData = TextBox(app)
-PushButton(app, text="Submit", command=guess)
 
-app.display()
+def runGame():
+    vars = setImageName("flags/")
+    makeWidget(vars[0], vars[1])
+
+
+if __name__ == "__main__":
+    runGame()
