60b8cffd74c2aa54f071bd08db90ac5418f03f2f
ZYPZ <72951371+ZYPZ33@users.noreply.github.com>
2023-09-20 08:16:27 +0100

    Added -l and better game over screen

--- flags.py
+++ flags.py
@@ -2,6 +2,7 @@
 from guizero import App, Picture, Text, TextBox, PushButton
 from random import choice
 from os import listdir
+from sys import argv
 
 directory = "flags/"
 points = 0
@@ -14,6 +15,11 @@ images.remove(imageName)
 image = directory + imageName
 
 
+def cheat(imageName):
+    if "-l" in argv:
+        print(imageName.strip(".jpg"))
+
+
 def guess():
     global imageName, images, points, turns
     if inputData.value.lower() == imageName.strip(".jpg").lower() and turns != 0:
@@ -21,7 +27,7 @@ def guess():
             imageName = choice(images)
             images.remove(imageName)
             flag.image = directory + imageName
-            # cheat.value = imageName.strip(".jpg")
+            cheat(imageName)
             label.value = "What country does this flag represent?"
         else:
             flag.destroy()
@@ -35,18 +41,18 @@ def guess():
     else:
         if turns == 1:
             turns -= 1
-            label.value = "Game over"
-        elif turns > 1:
+            label.value = "Game over\nAnswer was: " + imageName.strip(".jpg")
+            inputData.destroy()
+            button.destroy()
+        else:
             label.value = "Try again"
             turns -= 1
-        else:
-            label.value = "Game over"
         scores.value = f"Score: {points} | Turns:{turns}"
 
 
 app = App(title="Guess the Flag", width=300, height=400)
 scores = Text(app, f"Score: {points} | Turns:{turns}")
-# cheat = Text(app, imageName.strip(".jpg"))
+cheat(imageName)
 flag = Picture(app, image, width=150, height=100)
 label = Text(app, "What country does this flag represent?")
 inputData = TextBox(app)
