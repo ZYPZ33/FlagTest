37c9ca99b9757b79694241a1741931b0d43875af
ZYPZ <72951371+ZYPZ33@users.noreply.github.com>
2023-09-20 10:42:12 +0100

    Added desktop file && removed debug text

--- flags.py
+++ flags.py
@@ -5,56 +5,67 @@ from os import listdir
 from sys import argv
 
 directory = "flags/"
-points = 0
-turns = 10
-
 images = listdir(directory)
-
 imageName = choice(images)
 images.remove(imageName)
 image = directory + imageName
 
 
-def cheat(imageName):
-    if "-l" in argv:
-        print(imageName.strip(".jpg"))
-
-
-def guess():
-    global imageName, images, points, turns
-    if inputData.value.lower() == imageName.strip(".jpg").lower() and turns != 0:
+def guess(inputData, imageName, flag, label, cheat, turncount, scores, points):
+    if inputData.value.lower() == cheat.value.lower() and turncount.value != "0":
         if len(images) > 0:
             imageName = choice(images)
             images.remove(imageName)
             flag.image = directory + imageName
-            cheat(imageName)
+            cheat.value = imageName.strip(".jpg")
             label.value = "What country does this flag represent?"
         else:
             flag.destroy()
-            # cheat.destroy()
             inputData.destroy()
-            button.destroy()
             label.value = "You got all the flags right!"
         points += 1
-        scores.value = f"Score: {points} Turns:{turns}"
+        scores.value = f"Score: {points} Turns:{turncount.value}"
         inputData.value = ""
     else:
-        if turns == 1:
-            turns -= 1
+        if turncount.value <= "1":
+            turncount.value = int(turncount.value) - 1
             label.value = "Game over\nAnswer was: " + imageName.strip(".jpg")
             inputData.destroy()
-            button.destroy()
         else:
+            print(turncount.value)
             label.value = "Try again"
-            turns -= 1
-        scores.value = f"Score: {points} | Turns:{turns}"
+            turncount.value = int(turncount.value) - 1
+        scores.value = f"Score: {points} | Turns:{turncount.value}"
 
 
-app = App(title="Guess the Flag", width=300, height=400)
-scores = Text(app, f"Score: {points} | Turns:{turns}")
-cheat(imageName)
-flag = Picture(app, image, width=150, height=100)
-label = Text(app, "What country does this flag represent?")
-inputData = TextBox(app)
-button = PushButton(app, text="Submit", command=guess)  # stuck here
-app.display()
+def runGame(points, turns):
+    app = App(title="Guess the Flag", width=300, height=400)
+    scores = Text(app, f"Score: {points} | Turns:{turns}")
+    turncount = Text(app, turns)
+    turncount.hide()
+    cheat = Text(app, imageName.strip(".jpg"))
+    if "-l" not in argv:
+        cheat.hide()
+    flag = Picture(app, image, width=150, height=100)
+    label = Text(app, "What country does this flag represent?")
+    inputData = TextBox(app)
+    PushButton(
+        app,
+        text="Submit",
+        command=guess,
+        args=[
+            inputData,
+            imageName,
+            flag,
+            label,
+            cheat,
+            turncount,
+            scores,
+            points,
+        ],
+    )
+    app.display()
+
+
+if __name__ == "__main__":
+    runGame(0, 10)
--- install.sh
+++ install.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 executable_name="flagsGame"
 install_directory="/usr/local/bin"
+desktop_directory="/usr/share/applications/"
 sed 's/flags\//\/usr\/share\/flags\//g' flags.py > flagsInstall.py
+input="y"
 test -e ${install_directory}/${executable_name}&& \
   read -p "Do you want to replace the already installed version [Y/n]" input
 
 if [ "${input}" = "y" ] || [ "${input}" = "Y" ] || [ "${input}" = "" ]
 then install flagsInstall.py ${install_directory}/${executable_name}&&\
   cp -r flags/ /usr/share/flags/&&\
+  cp flags.desktop ${desktop_directory}
   echo "${executable_name} installed at ${install_directory}"
 fi
